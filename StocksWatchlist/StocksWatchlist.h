3bdada48251fa9d372de3220f75a98d838b1498d
Peña Fernando <pena.fernan@gmail.com>
2022-02-23 15:19:39 -0300

    Create StocksWatchlist framework

---
+++ StocksWatchlist/StocksWatchlist.h
@@ -0,0 +1,18 @@
+//
+//  StocksWatchlist.h
+//  StocksWatchlist
+//
+//  Created by Fernando Pena on 2/23/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StocksWatchlist.
+FOUNDATION_EXPORT double StocksWatchlistVersionNumber;
+
+//! Project version string for StocksWatchlist.
+FOUNDATION_EXPORT const unsigned char StocksWatchlistVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StocksWatchlist/PublicHeader.h>
+
+
